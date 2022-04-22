a303e7d0ca103fe5d43ed1d75c9d432bf0f58f94
Belyaev-MP <m.belyaev.sstu@gmail.com>
2022-04-22 22:25:54 +0400

    Spring starter.spring.io initial

---
+++ src/main/java/my/self/springapp/SpringWebAppIbsApplication.java
@@ -0,0 +1,13 @@
+package my.self.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppIbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppIbsApplication.class, args);
+	}
+
+}
